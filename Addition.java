d859eafab154a08afe31534910db7eb61ba8dc22
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-23 16:40:15 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,16 @@
+package smartgraduate;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter two number");
+		int first=100;
+		int second=200;
+		System.out.println(first+" "+second);
+		int sum=first+second;
+		System.out.println("The sum is:"+sum);
+
+	}
+
+}
