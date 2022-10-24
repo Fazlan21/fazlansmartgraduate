fed9fdd9dde0bf60b765e68fa74104e958127108
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-24 17:28:00 +0530

    Add files via upload

---
+++ QuotientRemainder.java
@@ -0,0 +1,17 @@
+package smartgraduate;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int dividend = 25,divisor = 4;
+         int quotient = dividend / divisor;
+         int reminder = dividend % divisor;
+         
+         
+         System.out.print("Quotient = "+ quotient);
+         System.out.print("Reminder="+reminder);
+         
+	}
+
+}
