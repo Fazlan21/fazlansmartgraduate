43a8faf14f2c8f76d00b1082b0286ae5d841820c
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-26 12:42:53 +0530

    Add files via upload

---
+++ FindAreaOfRectangle.java
@@ -0,0 +1,35 @@
+package smartgraduate;
+
+import java.util.Scanner;
+
+public class FindAreaOfRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+		   Scanner scanner = new Scanner(System.in);
+		   System.out.print("Enter the length of Rectangle:");
+		   double length = scanner.nextDouble();
+		   System.out.print("Enter the width of Rectangle:");
+		   double width = scanner.nextDouble();
+		   
+		   double area = length*width;
+		   System.out.print("Area of Rectangle is:"+area);
+		}
+	}
+}
+		   
+		   
+		   
+		   
+		   
+		   
+	
+		
+		
+		
+		
+
+	
+
+
