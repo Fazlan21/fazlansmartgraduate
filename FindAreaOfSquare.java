d31307fc356c860aaec9189708be102700c5ed5a
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-26 21:17:30 +0530

    Add files via upload

---
+++ FindAreaOfSquare.java
@@ -0,0 +1,27 @@
+package smartgraduate;
+
+import java.util.Scanner;
+
+public class FindAreaOfSquare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			System.out.print("Enter Side of Square:");
+			 
+			Scanner scanner = new Scanner(System.in);
+			   
+			double side = scanner.nextDouble();
+			
+			double area = side*side;
+			System.out.print("Area of Square is:"+area);
+		}
+	}
+}	
+		
+			
+		
+
+	
+
+
