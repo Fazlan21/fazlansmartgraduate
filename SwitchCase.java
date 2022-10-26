3d2e022bb126128a77b88762ca4c0901e388e2f6
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-26 12:19:30 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,27 @@
+package smartgraduate;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tech = 2;
+		
+		switch(tech) {
+		case 1:
+		{
+			 System.out.print("java");
+			 break;
+		}
+		case 2:{
+			  System.out.print("ES6");
+			  break;
+		}
+		default:{
+			System.out.print("Not Listed..");
+			
+		}  
+		}
+		}
+	
+
+	}
