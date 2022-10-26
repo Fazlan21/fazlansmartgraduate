d23e3b129cb3cb2e38da15e255be42a62887b24c
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-26 12:13:44 +0530

    Add files via upload

--- WhileLoopDeme.java
+++ WhileLoopDeme.java
@@ -1,10 +1,26 @@
 package smartgraduate;
 
+import java.util.Scanner;
+
 public class WhileLoopDeme {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int number, sum = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("n Please Enter any integer value below 10:");
+		number = sc.nextInt();
+		
+		while(number <= 10) {
+			sum = sum + number;
+			number++;
+		}	
+		System.out.format("Sum of the Numbers From the While Loop is: %d ",sum);
 	}
+}	
+		
+		
 
-}
+	
+
