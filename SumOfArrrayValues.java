e025ab583843f6a7ecbe225d3a7f61820a2cf1b7
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-27 12:41:15 +0530

    Add files via upload

---
+++ SumOfArrrayValues.java
@@ -0,0 +1,20 @@
+package smartgraduate;
+
+public class SumOfArrrayValues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {10, 20, 30, 40, 50, 10};
+		int sum = 0;
+		 
+		for(int num : array) {
+			 sum = sum+num;
+		}
+		System.out.print("Sum of array elements is:" + sum);
+	}
+}		
+		
+
+	
+
+
