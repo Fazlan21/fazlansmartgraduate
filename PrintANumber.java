233e2efade91d99d5dc53f2a438f127484832b7e
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-27 07:54:28 +0530

    Add files via upload

---
+++ PrintANumber.java
@@ -0,0 +1,21 @@
+package smartgraduate;
+
+import java.util.Scanner;
+
+public class PrintANumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Enter a number:");
+		
+		int number = reader.nextInt();
+		
+		System.out.print("You entered: " + number);
+	}
+}	
+		
+				
+
+	
+
