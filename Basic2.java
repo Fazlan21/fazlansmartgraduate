c1c427bca698afb3ab4f0d53f11fadc8229ec6ae
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-27 07:45:38 +0530

    Add files via upload

---
+++ Basic2.java
@@ -0,0 +1,22 @@
+package smartgraduate;
+
+import java.util.Scanner;
+
+public class Basic2 {
+
+	public static void main(String[] args) {
+		   Scanner scan=new Scanner(System.in);
+		   
+		   System.out.print("Enter value for i..:");
+		   int i=scan.nextInt();
+		   System.out.print("Enter value for j...:");
+		   int j=scan.nextInt();
+		   int mul=i*j;
+		   System.out.print("The Multiplication of i and j is ..:"+mul);
+		   
+		   
+		   
+		
+	}
+
+}
