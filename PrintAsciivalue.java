d09362104c742817021c00f4bc86c40fb41164a3
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-27 08:00:14 +0530

    Add files via upload

---
+++ PrintAsciivalue.java
@@ -0,0 +1,20 @@
+package smartgraduate;
+
+public class PrintAsciivalue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='H';
+		int ascii = ch;
+	    int castAscii=(int)ch;
+	    
+		
+		System.out.print("The ASCII value of " + ch + " is:"+ascii);
+	    System.out.print("The ASCII value of " + ch + " is: "+ castAscii);
+	    
+	    
+	    }
+         
+}
+
+
