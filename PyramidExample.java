17545074c896d5db64187fd73b7afabaca3eb8db
Fazlan21 <114311078+Fazlan21@users.noreply.github.com>
2022-10-27 08:03:11 +0530

    Add files via upload

---
+++ PyramidExample.java
@@ -0,0 +1,24 @@
+package smartgraduate;
+
+public class PyramidExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j;
+		for(int i=1;i<=5;i++){
+			   for(int j1=1;j1<=i;j1++){
+			         System.out.print("* ");
+		             }
+		   System.out.print(0);
+		}
+	}
+}	
+		
+		
+	
+
+
+		
+
+	
+
